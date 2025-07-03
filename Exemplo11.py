0b88335b8a91466b608198d32d011fb78be879c7
P-h-silva <mister.ps667@gmail.com>
2025-07-03 16:33:13 -0300

    Exemplo Python

---
+++ Exemplo11.py
@@ -0,0 +1,12 @@
+for i in range(10):
+    print("o valor de i é:", i)
+
+    print ("===========")
+
+    for i in range(1, 6):
+        print (i)
+
+print("==============")
+
+for i in range (2,21,3):
+    print("O valor de i é:", 1)
---
+++ Exemplo12.py
@@ -0,0 +1,6 @@
+import time
+for segundo in range(1, 11):
+  print ( segundo, "contando")
+  time.sleep(1)
+
+print ("Estou pronto! +D")
