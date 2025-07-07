0ac071e8d74ce0c504f5a3fe14924f5b0097244c
P-h-silva <mister.ps667@gmail.com>
2025-07-07 13:45:41 -0300

    Aula 6

---
+++ Aula 6/exemplo1.py
@@ -0,0 +1,8 @@
+minha_lista = [1,2,3,4,5]
+print (minha_lista)
+print (minha_lista[3])
+minha_lista[0] = 9
+print (minha_lista)
+
+tamanho = len(minha_lista)
+print ("O tamanho da lista é", tamanho)
